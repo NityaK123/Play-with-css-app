83bffa6c1a5a58c445dd22c98eee3d086bee8ae7
Nityanand Gupta <nitya20111997@gmail.com>
2025-02-12 22:23:42 +0530

    ok

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,32 +1,40 @@
-import React from 'react'
+import React,{useState} from 'react'
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import NavDropdown from 'react-bootstrap/NavDropdown';
 import Container from 'react-bootstrap/Container'
 import Image from 'react-bootstrap/Image'
 
+function App() {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
 
-const NavBar = () => {
+  const toggleMenu = () => {
+    setIsMenuOpen(!isMenuOpen);
+  };
 
-    return (
-        <Navbar className="bgColor">
-            <Container className="d-flex justify-content-between">
-                <Navbar.Brand href="#home">
-                    <Image src="./assets/playcss.png" className="playcss" alt="Play CSS" />
-                </Navbar.Brand>
-                <Nav className="d-flex justify-content-between">
-                    <Nav.Link href="#home">Features</Nav.Link>
-                    <Nav.Link href="#link">Pro</Nav.Link>
-                    <Nav.Link href="#link">Daily Submission</Nav.Link>
-                    <NavDropdown title="Challenges" id="basic-nav-dropdown">
-                        <NavDropdown.Item href="#action/3.1">Image Challenges</NavDropdown.Item>
-                        <NavDropdown.Item href="#action/3.3">UI Challenges</NavDropdown.Item>
-                    </NavDropdown>
-                    <Nav.Link href="#link">Sign Up</Nav.Link>
-                    <Nav.Link href="#link">Sign In</Nav.Link>
-                </Nav>
-            </Container>
-        </Navbar>
-    )
+  return (
+    <Navbar className="bgColor">
+      <Container className="d-flex justify-content-between">
+        <Navbar.Brand href="#home">
+          <Image src="./assets/playcss.png" className="playcss" alt="Play CSS" />
+        </Navbar.Brand>
+        <div className={`hamburger ${isMenuOpen ? 'active' : ''}`} onClick={toggleMenu}>
+          <div className="hamburgerIcon"></div>
+        </div>
+        <Nav className={`${isMenuOpen ? 'navbar-nav-active' : ''}`}>
+          <Nav.Link href="#home">Features</Nav.Link>
+          <Nav.Link href="#link">Pro</Nav.Link>
+          <Nav.Link href="#link">Daily Submission</Nav.Link>
+          <NavDropdown title="Challenges" id="basic-nav-dropdown">
+            <NavDropdown.Item href="#action/3.1">Image Challenges</NavDropdown.Item>
+            <NavDropdown.Item href="#action/3.3">UI Challenges</NavDropdown.Item>
+          </NavDropdown>
+          <Nav.Link href="#link">Sign Up</Nav.Link>
+          <Nav.Link href="#link">Sign In</Nav.Link>
+        </Nav>
+      </Container>
+    </Navbar>
+  );
 }
-export default NavBar
+
+export default App;
